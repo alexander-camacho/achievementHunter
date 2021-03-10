d600ed5eb01e1b61e50dab4e6e8872c8fc8f6cdb
Alex Camacho <alsbrain@optonline.net>
2021-03-09 19:26:41 -0800

    added wowhead link to achievement banner

--- client/src/components/AchieveBanner/index.js
+++ client/src/components/AchieveBanner/index.js
@@ -1,9 +1,15 @@
 import React from "react"
 
 function AchieveBanner(props) {
-    const name = props.data.achievement.name
+    console.log(props)
+
+    const url = `//wowhead.com/achievement=${props.data?.randomAchieve?.achievement?.id}`
+
+    const name = props.data?.randomAchieve?.achievement?.name
+
     return (
-        <div className="achievement">{name}
+        <div className="achievement">
+            <a target="_blank}" href={url}>{name}</a>
 
             {/* save button */}
             <button onClick={() => console.log('test')}>Save</button>
