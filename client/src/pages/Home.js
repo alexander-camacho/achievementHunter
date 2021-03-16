deb15090ca669f43a82148f7bcf620df495d81a9
Alex Camacho <alsbrain@optonline.net>
2021-03-15 22:46:04 -0700

    moved form logic into search form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,52 +1,21 @@
-import React, { Component } from "react"
+import React from "react"
 import SearchForm from "../components/SearchForm"
+import { useStoreContext } from "../utils/GlobalState"
 
 
+const Home = () => {
 
-
-class Home extends Component {
-
-    state = {
-        realm: "",
-        character: ""
-    }
-
-    handleRealm = event => {
-        this.setState({
-            realm: String(event.target.value).toLowerCase()
-        });
-    };
-
-    handleChar = event => {
-        this.setState({
-            character: String(event.target.value).toLowerCase()
-        });
-    };
-
-    handleFormSubmit = event => {
-        event.preventDefault()
-        console.log(`Character: ${this.state.character}`)
-        console.log(`Realm: ${this.state.realm}`)
-
-    }
-
-    render() {
 
         return (<div className="container">
             <h1>Welcome to Random Achievement Hunter</h1>
             <p>Please enter your character's name and server below to get started!</p>
 
-            <SearchForm
-                handleFormSubmit={this.handleFormSubmit}
-                handleRealm={this.handleRealm}
-                handleChar={this.handleChar}
-            />
+            <SearchForm/>
 
 
         </div>
         )
     }
 
-}
 
 export default Home
