c14056a6015cf0380577bb3ffb57f674bb23af91
Alex Camacho <alsbrain@optonline.net>
2021-03-17 21:42:57 -0700

    added functionality to clear state when home page is loaded

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,17 @@
-import React from "react"
+import React, { useEffect } from "react"
 import SearchForm from "../components/SearchForm"
+import { CLEAR_DATA, LOADING } from "../utils/actions"
 import { useStoreContext } from "../utils/GlobalState"
 
 
 const Home = () => {
 
+    const [state, dispatch] = useStoreContext()
+
+    useEffect(() => {
+        dispatch({ type: LOADING })
+        dispatch({ type: CLEAR_DATA })
+    }, [])
 
         return (<div className="container">
             <h1>Welcome to Random Achievement Hunter</h1>
